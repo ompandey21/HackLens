2063248d30ce2acacf00790e7ac51afbc2c8994b
ompandey21 <omdutt2004@gmail.com>
2025-10-29 22:54:13 +0530

    CORS Integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.routes import auth, users
+from app.routes import model_evaluator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(model_evaluator.router, prefix="/model_evaluator", tags=["Model Evaluator"])
 
 @app.get("/")
 async def root():
